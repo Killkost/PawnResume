d89708eba67d77479ace96f9bcfa190d8741906e
Killkost <110656800+Killkost@users.noreply.github.com>
2026-03-01 12:03:56 +0200

    Refactor spawnnewmodel.lua for improved readability

--- spawnnewmodel.lua
+++ spawnnewmodel.lua
@@ -4,9 +4,9 @@ function main()
     while true do
         wait(0)
         
-        -- Телепорт и проверка модели по нажатию K
+
         if isKeyJustPressed(vkeys.VK_K) then
-            setCharCoordinates(PLAYER_PED, 0.0, 0.0, 5.0) -- Тп чуть выше нуля, чтоб не провалиться
+            setCharCoordinates(PLAYER_PED, 0.0, 0.0, 5.0) 
             if isModelAvailable(5500) then
                 print("Model 5500 is AVAILABLE in game files!")
             else
@@ -15,7 +15,7 @@ function main()
             print("Teleported to 0, 0, 5")
         end
 
-        -- Твой допиленный код для спавна объекта на L
+
         if isKeyJustPressed(vkeys.VK_L) then
             lua_thread.create(function() 
                 local myModelId = 19600
@@ -26,15 +26,15 @@ function main()
                     wait(0) 
                 end
                 
-                -- Проверяем, не пустой ли ID
+
                 if hasModelLoaded(myModelId) then
                     local x, y, z = getCharCoordinates(PLAYER_PED)
-                    -- Создаем объект прямо перед собой
+
                     local myObj = createObject(myModelId, x + 2.0, y, z)
                     
                     if doesObjectExist(myObj) then
                         print("Object spawned SUCCESSFULLY!")
-                        -- Если хочешь, чтобы он не падал, можно заморозить
+
                         freezeObjectPosition(myObj, true)
                         markModelAsNoLongerNeeded(myModelId)
                     else
@@ -44,4 +44,5 @@ function main()
             end)
         end
     end
+
 end
