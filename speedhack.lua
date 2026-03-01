38d354cd615e4ec73266117ce100c90a3690f531
Killkost <110656800+Killkost@users.noreply.github.com>
2026-03-01 12:12:02 +0200

    Refactor speedhack.lua for improved readability

--- speedhack.lua
+++ speedhack.lua
@@ -1,33 +1,30 @@
 local vkeys = require 'vkeys'
 
--- Настройка силы ускорения (чем выше число, тем сильнее рывок)
+
 local speed_multiplier = 0.5 
 
 function main()
     if not isSampLoaded() or not isSampfuncsLoaded() then return end
     while not isSampAvailable() do wait(100) end
 
-    sampAddChatMessage("{00FF00}[Cheat]{FFFFFF} SpeedHack загружен. Зажми {FFFF00}LSHIFT{FFFFFF} в машине.", -1)
+    sampAddChatMessage("{00FF00}[Cheat]{FFFFFF} SpeedHack Г§Г ГЈГ°ГіГ¦ГҐГ­. Г‡Г Г¦Г¬ГЁ {FFFF00}LSHIFT{FFFFFF} Гў Г¬Г ГёГЁГ­ГҐ.", -1)
 
-    -- ВЕЧНЫЙ ЦИКЛ
+
     while true do
-        wait(0) -- Даем игре работать
+        wait(0) 
 
-        -- Проверяем: игрок в машине и зажат Левый Шифт
+        
         if isCharInAnyCar(PLAYER_PED) and isKeyDown(vkeys.VK_LSHIFT) then
-            -- Получаем хендл машины, в которой сидит игрок
+            
             local vehicle = storeCarCharIsInNoSave(PLAYER_PED)
             
             if doesVehicleExist(vehicle) then
-                -- Получаем текущую скорость машины
-                --local cur_speed = getCarForwardSpeed(vehicle)
-                
-                -- Устанавливаем новую скорость (текущая + надбавка)
+
                 setCarForwardSpeed(vehicle, 100)
                 
-                -- Опционально: можно добавить визуальный эффект или звук
-                -- printStringNow("~g~SPEEDHACK ACTIVE", 100)
+
             end
         end
     end
+
 end
